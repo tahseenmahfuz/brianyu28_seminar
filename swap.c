7a7de340ae6afd940b1161391ed9079fa9287930
Brian Yu <brianyu28@gmail.com>
2016-11-07 16:30:09 +0000

    Add swap file

---
+++ swap.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x = 28;
+    int y = 50;
+    
+    printf("x is %i\n", x);
+    printf("y is %i\n", y);
+    
+    // swapping...
+    
+    printf("x is now %i\n", x);
+    printf("y is now %i\n", y);
+}
