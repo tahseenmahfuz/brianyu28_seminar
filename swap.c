030a516f24824bf11aceadecd6645247d5664841
Brian Yu <brianyu28@gmail.com>
2016-11-07 20:47:58 +0000

    Actually swap the values

--- swap.c
+++ swap.c
@@ -9,6 +9,9 @@ int main(void)
     printf("y is %i\n", y);
     
     // swapping...
+    int temp = x;
+    x = y;
+    y = temp;
     
     printf("x is now %i\n", x);
     printf("y is now %i\n", y);
