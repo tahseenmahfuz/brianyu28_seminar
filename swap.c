217f5337aea80968611448dba201cb2ecbdbc64b
Tahseen Haque <tahseenmahfuz@gmail.com>
2017-10-10 09:43:26 -0400

    changed variable names

--- swap.c
+++ swap.c
@@ -2,17 +2,17 @@
 
 int main(void)
 {
-    int x = 34;
-    int y = 50;
-    
+    int a = 34;
+    int b = 50;
+
     printf("x is %i\n", x);
     printf("y is %i\n", y);
-    
+
     // swapping...
     int temp = x;
-    x = y;
-    y = temp;
-    
+    a = b;
+    b = temp;
+
     printf("x is now %i\n", x);
     printf("y is now %i\n", y);
 }
